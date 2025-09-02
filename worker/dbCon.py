004af2dd446574483bb262e6db2f988ffe4f6d82
Anawat <66070217@kmitl.ac.th>
2025-09-02 11:37:07 +0700

    feat: finish state4

---
+++ worker/dbCon.py
@@ -0,0 +1,20 @@
+import os, datetime
+
+from pymongo import MongoClient
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+def upload_interface(ip, status):
+    client = MongoClient(MONGO_URI)
+    database = client[DB_NAME]
+    interface = database["interface_status"]
+
+    current = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    result = interface.insert_one({
+        "router_ip": ip,
+        "timestamp": current,
+        "interfaces": status
+    })
+
+    return result
---
+++ worker/router_controller.py
@@ -0,0 +1,19 @@
+from netmiko import ConnectHandler
+
+class Rcontroller:
+    def __init__(self, host, username="admin", password="cisco"):
+        self.config = {
+            'device_type': 'cisco_ios',
+            'host': host,
+            'username': username,
+            'password': password
+        }
+    
+    def makeConnection(self):
+        connection = ConnectHandler(**self.config)
+        return connection
+
+if __name__ == "__main__":
+    controller = Rcontroller('10.30.6.19')
+    with controller.makeConnection() as connection:
+        print(connection.send_command('show ip int br', use_textfsm=True))
